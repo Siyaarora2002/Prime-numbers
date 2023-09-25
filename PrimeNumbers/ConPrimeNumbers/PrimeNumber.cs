f75cc0e8a25c57e02374945e6aa281475966d97d
Siya Arora <145938796+Siyaarora2002@users.noreply.github.com>
2023-09-25 09:29:01 -0400

    Add files via upload

---
+++ PrimeNumbers/ConPrimeNumbers/PrimeNumber.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Threading;
+
+namespace PrimeNumber
+{
+    public class PrimeNumber
+    {
+        private CancellationTokenSource cancellationTokenSource;
+        private ManualResetEvent manualResetEvent;
+        private bool paused;
+        private bool restart;
+
+        public PrimeNumber(CancellationTokenSource cancellationTokenSource, ManualResetEvent manualResetEvent)
+        {
+            this.cancellationTokenSource = cancellationTokenSource;
+            this.manualResetEvent = manualResetEvent;
+            this.paused = false;
+            this.restart = false;
+        }
+
+        public void GeneratePrimeNumbers()
+        {
+            int number = 0;
+
+            while (true)
+            {
+                if (cancellationTokenSource.IsCancellationRequested)
+                    break;
+
+                if (!paused)
+                {
+                    if (IsPrime(number))
+                    {
+                        Console.Write($"{number} ");
+                        Thread.Sleep(1000);
+                    }
+
+                    number++;
+
+                    if (restart)
+                    {
+                        number = 1;
+                        restart = false;
+                    }
+                }
+
+                manualResetEvent.WaitOne();
+            }
+        }
+
+
+        private bool IsPrime(int number)
+        {
+            if (number <= 1)
+                return false;
+
+            for (int i = 2; i * i <= number; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        public void PauseGeneration()
+        {
+            paused = true;
+            Console.WriteLine("Printing has been Paused");
+        }
+
+        public void ResumeGeneration()
+        {
+            paused = false;
+            manualResetEvent.Set();
+            Console.WriteLine("Printing has been Restarted");
+        }
+
+        public void ResetGeneration()
+        {
+            paused = false;
+            restart = true;
+            manualResetEvent.Set();
+            Console.WriteLine("Printing Pime Numbers from the beginning");
+        }
+
+        public void QuitGeneration()
+        {
+            cancellationTokenSource.Cancel();
+            Console.WriteLine("Quitting Application");
+            Console.WriteLine("Thread is interrupted");
+        }
+    }
+}
---
+++ PrimeNumbers/ConPrimeNumbers/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press s to start Printing Pime Numbers");
+            Console.WriteLine("Press x to pause Printing Pime Numbers");
+            Console.WriteLine("Press a to restart Printing prime numbers");
+            Console.WriteLine("Press r to restart Printing prime numbers from the beginning");
+            Console.WriteLine("Press q to quit the application");
+
+
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            ManualResetEvent manualResetEvent = new ManualResetEvent(true);
+
+            PrimeNumber primeNumber = new PrimeNumber(cancellationTokenSource, manualResetEvent);
+            Thread primeThread = new Thread(primeNumber.GeneratePrimeNumbers);
+            primeThread.Start();
+
+            ConsoleKeyInfo key;
+            do
+            {
+                
+                key = Console.ReadKey(true);
+
+                switch (key.KeyChar)
+                {
+                    case 's':
+                        manualResetEvent.Set();
+                        break;
+                    case 'x':
+                        primeNumber.PauseGeneration();
+                        break;
+                    case 'a':
+                        primeNumber.ResumeGeneration();
+                        break;
+                    case 'r':
+                        primeNumber.ResetGeneration();
+                        break;
+                    case 'q':
+                        primeNumber.QuitGeneration();
+                        break;
+                }
+            } while (key.KeyChar != 'q');
+        }
+    }
+}
